f3d038c6a6e4f08b9c8a42f25b00df1e0f3ea52d
Gustavo-jms36 <gustavo.jms36@gmail.com>
2022-06-14 17:18:53 -0500

    Imprimiendo tama;o de tipos de datos

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 
 	printf("Size of a int: %lu byte(s)\n", sizeof(int));
 	printf("Size of an char: %lu byte(s)\n", sizeof(char));
-	printf("Size of a long: %lu byte(s)\n", sizeof(long)
+	printf("Size of a long: %lu byte(s)\n", sizeof(long);
 	printf("Size of a double: %lu byte(s)\n", sizeof(double));
 
 	return (0);
