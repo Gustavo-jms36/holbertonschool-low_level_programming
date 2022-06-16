ee12a76c00d48a526a8e2e2b6296aff54064fe1e
Gustavo-jms36 <gustavo.jms36@gmail.com>
2022-06-15 23:09:27 -0500

    abecedario en mayusculas y minusculas

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int letter = 65;
+
+	while
+		(letter <= 124);
+	{
+		putchar(letter);
+		putchar('\n');
+		(letter++);
+	}
+	return (0);
+}
